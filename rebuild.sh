0ae060770c174bf8eea5aacd19349260a0471909
code-fusheng <2561035977@qq.com>
2022-11-17 01:23:58 +0800

    版本精简

--- rebuild.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-# 如果 MAC 提示 zsh: permission denied: ./rebuild.sh
-# 请执行 chmod u+x rebuild.sh
-rm -rf target/*
-mvn -Dmaven.test.skip=true clean package
-rm -rf target/*.gz
-
--- sync_pro.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#密码:zH1314520?
-#rsync -avz --progress --exclude=logs --exclude=.git . root@175.24.45.179:/usr/cloud/model-prod/model
-rsync -avz --progress --exclude=logs --exclude=.git . root@47.111.158.6:/root/App/model
